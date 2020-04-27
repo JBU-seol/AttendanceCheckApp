49d1376c32d0a498a72fe8584f59ea818fec49c0
JBU-seol <seolki100480@gmail.com>
2020-04-27 15:29:42 +0900

    New Mac Address Function

--- Login.js
+++ Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View, StatusBar, TextInput, TouchableOpacity,
-Dimensions } from 'react-native';
+Dimensions, Alert } from 'react-native';
+import * as Permission from 'expo-permissions';
+import * as Network from 'expo-network';
 
 const {width, height} = Dimensions.get("window");
 
@@ -10,8 +12,23 @@ export default class Login extends React.Component {
         macAddress: ""
     }
 
+    getMac = async() => {
+        try{
+            const mac = await Network.getMacAddressAsync("wlan0");
+            this.setState({
+                macAddress: mac
+            })
+        } catch(error){
+            Alert.alert("Can't find your interface","Error")
+        }
+
+    };
+
+    componentDidMount() {
+        this.getMac();
+    };
+
     render() {
-        console.log(this.state.macAddress)
         return (
             <View style={styles.container}>
             <StatusBar barStyle="light-content" />
@@ -21,17 +38,20 @@ export default class Login extends React.Component {
                     <TextInput style={styles.inputcard} 
                     maxLength={10}
                     keyboardType={"number-pad"}
-                    placeholder={" Your Student Code  ex ) 91512345"}
-                    placeholderTextColor={"white"}
+                    placeholder={"Your Student Code  ex ) 91512345"}
                     value={this.state.studentCode}
                     onChangeText={this._editInput}
                     returnKeyType={"done"}
                     />
                     <Text style={{fontSize:20}}> </Text>
-                    <TextInput style={styles.inputcard} />
+                    <TextInput style={styles.inputcard}
+                    placeholder={this.state.macAddress}
+                    placeholderTextColor={"white"}
+                    />
                 </View>
                 <View style={styles.subcontainer3}>
-                    <TouchableOpacity style={styles.loginbox}>
+                    <TouchableOpacity style={styles.loginbox}
+                    onPress={()=>{Alert.alert("Login !")}}>
                         <Text style={styles.logintext}>
                             Login
                         </Text>
@@ -46,6 +66,7 @@ export default class Login extends React.Component {
             studentCode: text
         })
     }
+
 }
 
 const styles = StyleSheet.create({
@@ -74,7 +95,7 @@ const styles = StyleSheet.create({
         fontStyle: "italic",
         width: width/1.3,
         height: height/20,
-        borderRadius: 3
+        borderRadius: 3,
     },
     loginbox: {
         backgroundColor: "gray",

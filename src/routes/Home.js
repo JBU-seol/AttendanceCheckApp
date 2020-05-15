4304cd668f7637c1e0c00fa0ebee2fc8c2e1407f
JBU-seol <seolki100480@gmail.com>
2020-05-15 15:31:34 +0900

    Getting Lectrue Name from Server

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -15,6 +15,11 @@ const getIdAPI = "http://ec2-13-125-176-205.ap-northeast-2.compute.amazonaws.com
 const getNameAPI = "http://ec2-13-125-176-205.ap-northeast-2.compute.amazonaws.com:1234/members/course_name";
 const getTimeAPI = "http://ec2-13-125-176-205.ap-northeast-2.compute.amazonaws.com:1234/members/course_time";
 
+const Monlec = []
+const Tuelec = []
+const Wenlec = []
+const Thulec = []
+//const Frilec = []
 export default class Home extends React.Component{
     static navigationOptions={
         headerShown : false
@@ -22,7 +27,8 @@ export default class Home extends React.Component{
 
     state = {
         LectureName : [],
-        LectureTime : []
+        LectureTime : [],
+        Frilec : []
     }
 
     _getLectureTime = async(id) => {
@@ -34,9 +40,9 @@ export default class Home extends React.Component{
                 }),
             })
             let responseJson = await response.json();
-            console.log(responseJson);
+            //console.log(responseJson[id]);
             this.setState({ 
-                LectureTime : this.state.LectureTime.concat(responseJson)
+                LectureTime : this.state.LectureTime.concat(responseJson[id])
             })
         } catch(error){
             Alert.alert(error)
@@ -52,10 +58,9 @@ export default class Home extends React.Component{
                 }),
             })
             let responseJson = await response.json();
-            console.log(responseJson);
+            //console.log(responseJson);
             this.setState( {
                 LectureName : this.state.LectureName.concat(responseJson)
-                //LectureName : responseJson
             })
             this._getLectureTime(responseJson.id);
         } catch(error){
@@ -82,13 +87,70 @@ export default class Home extends React.Component{
         }
     }
 
+    _settingLectureDay = ( lecture_id ) => {
+        //const lecture_obj = this.props.navigation.state.params;
+        const lecture_time = this.state.LectureTime;
+
+        for( var j=0 ; j < lecture_time.length ; j++ ){
+            if( lecture_id.id === lecture_time[j].id ){
+                switch( lecture_time[j].which_day ){
+                    case '월요일':
+                        Monlec.push(lecture_id.lecture_name)
+                        // this.setState({
+                        //     Monlec : this.state.Monlec.concat(lecture_id.lecture_name)
+                        // })
+                        //console.log("월요일");
+                        break;
+                    case '화요일':
+                        Tuelec.push(lecture_id.lecture_name)
+                        // this.setState({
+                        //     Tuelec : this.state.Tuelec.concat(lecture_id.lecture_name)
+                        // })
+                        //console.log("화요일");
+                        break;
+                    case '수요일':
+                        Wenlec.push(lecture_id.lecture_name)
+                        // this.setState({
+                        //     Wenlec : this.state.Wenlec.concat(lecture_id.lecture_name)
+                        // })
+                        //console.log("수요일");
+                        break;
+                    case '목요일':
+                        Thulec.push(lecture_id.lecture_name)
+                        // this.setState({
+                        //     Thulec : this.state.Thulec.concat(lecture_id.lecture_name)
+                        // })
+                        //console.log("목요일");
+                        break;
+                    case '금요일':
+                        //Frilec.push(lecture_id.lecture_name)
+                        this.setState( {
+                            Frilec : [ ...this.state.Frilec, lecture_id.lecture_name ]
+                        })
+                        // console.log(lecture_id.lecture_name);
+                        break;
+                }
+            }
+        }
+    }
+
+    _settingLecture = () => {
+        // const lecture_obj = this.props.navigation.state.params;
+        // console.log(lecture_obj);
+        const lecture_id = this.state.LectureName;
+        console.log(lecture_id)
+        for( var i=0 ; i < lecture_id.length ; i++ ){
+            this._settingLectureDay( lecture_id[i] )
+        }
+    }
+
     componentDidMount() {
         this._getLectureID();
+        //this._settingLecture();
     }
 
     render() {
-        
-        console.log(this.props.screenProps.code)
+        //console.log(this.props.screenProps.code)
         return (
             <View style={styles.container}>
                 <View style={styles.logocontainer}>
@@ -126,7 +188,7 @@ export default class Home extends React.Component{
                         <View style={styles.lecturechoicecard}>
                             <TouchableOpacity onPress={()=> this.props.navigation.navigate( 'Sub', {
                                 LectureName : this.state.LectureName,
-                                LectureTime : this.state.LectureTime
+                                LectureTime : this.state.LectureTime,
                             } )}>
                                 <Image style={styles.imagesetting} source={lecturechoice} />
                             </TouchableOpacity>
--- src/routes/Sub.js
+++ src/routes/Sub.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, TouchableOpacity, Dimensions, ScrollView } from
 
 const { width, height } = Dimensions.get("window");
 
+
 export default class Sub extends React.Component {
     static navigationOptions = {
         title: "출결조회",
@@ -13,58 +14,168 @@ export default class Sub extends React.Component {
     }
 
     state = {
-        isMon: true
+        isMon: true,
+        isTue: true,
+        isWen: true,
+        isThu: true,
+        isFri: true,
     }
 
-    togglefunc =() => {
+    toggleMon =() => {
         this.setState(prevState =>{
             return {
             isMon: !prevState.isMon
             }
         });
     }
+    toggleTue =() => {
+        this.setState(prevState =>{
+            return {
+            isTue: !prevState.isTue
+            }
+        });
+    }
+    toggleWen =() => {
+        this.setState(prevState =>{
+            return {
+            isWen: !prevState.isWen
+            }
+        });
+    }
+    toggleThu =() => {
+        this.setState(prevState =>{
+            return {
+            isThu: !prevState.isThu
+            }
+        });
+    }
+    toggleFri =() => {
+        this.setState(prevState =>{
+            return {
+            isFri: !prevState.isFri
+            }
+        });
+    }
 
+    
     render() {
-    console.log(this.props.navigation.state.params)
+        const { LectureName, LectureTime } = this.props.navigation.state.params;
+        let fri_list;
+        console.log(this.props.navigation.state.params)
       return (
           <ScrollView>
               <View style={styles.container}>
                   <View style={styles.daycard}>
-                      <Text style={styles.daytext} onPress={this.togglefunc}>
-                          월요일
-                  </Text>
-                      {this.state.isMon && (
+                      <Text style={styles.daytext} onPress={this.toggleMon}>
+                          월요일(
+                        </Text>
+                      {/* {this.state.isMon && (
                           <View style={styles.fadingContainer}>
-                              <TouchableOpacity onPress={() => this.props.navigation.navigate('SubDetail')}>
-                                  <Text style={styles.fadingText}>
-                                      정보보호프로젝트설계실습1
-                                </Text>
-                                  <Text style={{ marginLeft: 10, marginBottom: 5 }}>
-                                      13:10~14:00
-                                </Text>
-                              </TouchableOpacity>
+                              {
+                                  Monlec.map( (values, i) => {
+                                      return (
+                                          <TouchableOpacity key={i} onPress={() => this.props.navigation.navigate('SubDetail')}>
+                                              <Text style={styles.fadingText}>
+                                                  {values}
+                                              </Text>
+                                          </TouchableOpacity>
+                                      )
+                                  })
+                              }
                           </View>
-                      )}
+                      )
+                      } */}
                   </View>
                   <View style={styles.daycard}>
-                      <Text style={styles.daytext}>
+                      <Text style={styles.daytext} onPress={this.toggleTue}>
                           화요일
                   </Text>
+                  {/* {this.state.isTue && (
+                          <View style={styles.fadingContainer}>
+                              {
+                                  Tuelec.map( (values,i) => {
+                                      return (
+                                          <TouchableOpacity key={i} onPress={() => this.props.navigation.navigate('SubDetail')}>
+                                              <Text style={styles.fadingText}>
+                                                  {values}
+                                              </Text>
+                                          </TouchableOpacity>
+                                      )
+                                  })
+                              }
+                          </View>
+                      )
+                      } */}
                   </View>
                   <View style={styles.daycard}>
-                      <Text style={styles.daytext}>
+                      <Text style={styles.daytext} onPress={this.toggleWen}>
                           수요일
                   </Text>
+                  {/* {this.state.isWen && (
+                          <View style={styles.fadingContainer}>
+                              {
+                                  Wenlec.map((values,i) => {
+                                      return (
+                                          <TouchableOpacity key={i} onPress={() => this.props.navigation.navigate('SubDetail')}>
+                                              <Text style={styles.fadingText}>
+                                                  {values}
+                                              </Text>
+                                          </TouchableOpacity>
+                                      )
+                                  })
+                              }
+                          </View>
+                      )
+                      } */}
                   </View>
                   <View style={styles.daycard}>
-                      <Text style={styles.daytext}>
+                      <Text style={styles.daytext} onPress={this.toggleThu}>
                           목요일
                   </Text>
+                  {/* {this.state.isThu && (
+                          <View style={styles.fadingContainer}>
+                              {
+                                  Thulec.map((values,i) => {
+                                      return (
+                                          <TouchableOpacity key={i} onPress={() => this.props.navigation.navigate('SubDetail')}>
+                                              <Text style={styles.fadingText}>
+                                                  {values}
+                                              </Text>
+                                          </TouchableOpacity>
+                                      )
+                                  })
+                              }
+                          </View>
+                      )
+                      } */}
                   </View>
                   <View style={styles.daycard}>
-                      <Text style={styles.daytext}>
+                      <Text style={styles.daytext} onPress={this.toggleFri}>
                           금요일
                   </Text>
+                  {this.state.isFri && (
+                          <View style={styles.fadingContainer}>
+                              {
+                                  LectureTime.map( (value,i) => {
+                                      if( value.which_day === "금요일"){
+                                        // fri_id_list.push(value.id)
+                                        // console.log(fri_id_list)
+                                        LectureName.map( obj => {
+                                            if( obj.id === value.id ){
+                                                fri_list = obj.lecture_name
+                                            }
+                                        })
+                                        return (
+                                            <Text>
+                                                {fri_list}
+                                            </Text>
+                                        )
+                                      }
+                                  })
+                              }
+                          </View>
+                      )
+                      }
                   </View>
               </View>
           </ScrollView>
@@ -87,8 +198,8 @@ export default class Sub extends React.Component {
     daytext:{
         color: "#fff",
         paddingVertical: 5,
-        paddingHorizontal: width/2.28,
-        fontSize: 17,
+        paddingHorizontal: width/2.6,
+        fontSize: 18,
     },
     fadingContainer:{
         backgroundColor: "#fff",
